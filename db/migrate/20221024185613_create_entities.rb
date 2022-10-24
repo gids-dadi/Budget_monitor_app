6f7a44a9bc4d0af76070c42d5e3dbf7f4de6a41a
gids-dadi <gideons564@gmail.com>
2022-10-24 21:22:47 +0100

    Create migration and models of database

--- db/migrate/20221024185613_create_entities.rb
+++ db/migrate/20221024185613_create_entities.rb
@@ -1,6 +1,8 @@
 class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
+        t.string :name
+        t.decimal :amount
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_185943) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_194843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "entities", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
+    t.index ["group_id"], name: "index_entities_on_group_id"
     t.index ["user_id"], name: "index_entities_on_user_id"
   end
 
   create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
@@ -29,10 +35,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_185943) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "entities", "groups"
   add_foreign_key "entities", "users"
   add_foreign_key "groups", "users"
 end
