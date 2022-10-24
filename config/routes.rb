06a4e7353eb77cee71a8ef1ca70ff88c8a7a523b
gids-dadi <gideons564@gmail.com>
2022-10-24 22:56:06 +0100

    Add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'home/index'
+  get 'transactions/index'
+  get 'categories/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  root "home#index"
 end
