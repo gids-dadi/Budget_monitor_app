e2f56d26923dec2a35f528fd227d140d4f4f0c00
gids-dadi <gideons564@gmail.com>
2022-10-27 08:04:45 +0100

    Add views files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'menus/index'
   devise_for :users
+
+  authenticated :user do
+    root 'categories#index', as: :authenticated_root
+  end
+
  root 'home#index'
   
-  resources :categories, only: %i[index] do
-    resources :transactions, only: %i[index]
+  resources :categories, only: %i[index new show create destroy update edit] do
+    resources :transactions, only: %i[index new show create destroy update edit]
   end
+  resources :menus
 end
